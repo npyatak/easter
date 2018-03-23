01867a0bc3c958ac09803d58a93ce1b9d38f5cf4
LiamTorrelli <atleast24.7@list.ru>
2018-03-23 19:36:12 +0800

    Update 23 March 14:35

--- frontend/views/site/page1.php
+++ frontend/views/site/page1.php
@@ -1,1 +1,153 @@
-Куличи к Пасхе
+<div class="br-block">
+    <div class="text-center"><img class="icon" src="/images/general-page/kulich.png" alt="Kulich"></div>
+    <h2>Лучшие<br> рецепты пасхальных куличей</h2>
+    <div class="text-center">
+        <a class="simple-link text-white" href="">Смотреть все</a>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Кулич творожный с изюминкой</a></h3>
+                        <p>Для теста:</p>
+                        <p>1 упаковка (7г) Дрожжей Dr. Oetker, 200 мл теплого молока, 550 г муки, 3 яйца, 100 г сливочного масла, 180 г сахара, 1 щепотка соли, 1 упаковка (8г) Ванильного сахара Dr. Oetker, 1 упаковка (10г) Разрыхлителя теста Dr. Oetker. 50 г изюма, 2 ст.л. меда</p>
+                        <div class="actions">
+                            <a class="p-btn btn-rounded btn-red" href="">Как приготовить</a>
+                            <a class="simple-link" href="">Смотреть все</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Кулич творожный с изюминкой</a></h3>
+                        <p>Для теста:</p>
+                        <p>1 упаковка (7г) Дрожжей Dr. Oetker, 200 мл теплого молока, 550 г муки, 3 яйца, 100 г сливочного масла, 180 г сахара, 1 щепотка соли, 1 упаковка (8г) Ванильного сахара Dr. Oetker, 1 упаковка (10г) Разрыхлителя теста Dr. Oetker. 50 г изюма, 2 ст.л. меда</p>
+                        <div class="actions">
+                            <a class="p-btn btn-rounded btn-red" href="">Как приготовить</a>
+                            <a class="simple-link" href="">Смотреть все</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Кулич творожный с изюминкой</a></h3>
+                        <p>Для теста:</p>
+                        <p>1 упаковка (7г) Дрожжей Dr. Oetker, 200 мл теплого молока, 550 г муки, 3 яйца, 100 г сливочного масла, 180 г сахара, 1 щепотка соли, 1 упаковка (8г) Ванильного сахара Dr. Oetker, 1 упаковка (10г) Разрыхлителя теста Dr. Oetker. 50 г изюма, 2 ст.л. меда</p>
+                        <div class="actions">
+                            <a class="p-btn btn-rounded btn-red" href="">Как приготовить</a>
+                            <a class="simple-link" href="">Смотреть все</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Кулич творожный с изюминкой</a></h3>
+                        <p>Для теста:</p>
+                        <p>1 упаковка (7г) Дрожжей Dr. Oetker, 200 мл теплого молока, 550 г муки, 3 яйца, 100 г сливочного масла, 180 г сахара, 1 щепотка соли, 1 упаковка (8г) Ванильного сахара Dr. Oetker, 1 упаковка (10г) Разрыхлителя теста Dr. Oetker. 50 г изюма, 2 ст.л. меда</p>
+                        <div class="actions">
+                            <a class="p-btn btn-rounded btn-red" href="">Как приготовить</a>
+                            <a class="simple-link" href="">Смотреть все</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="br-block other-recipies">
+    <div class="text-center"><img class="icon" src="/images/general-page/kulich.png" alt="Kulich"></div>
+    <h2>Другие рецепты к Пасхе</h2>
+    <div class="text-center">
+        <a class="simple-link text-white" href="">Смотреть все</a>
+    </div>
+    <div class="container">
+        <div class="row other-recipies-slider">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Сладкие пасхальные яйца 1</a></h3>
+                        <img src="/images/main-slider/author-temp.jpg" alt="">
+                        <p class="author-name">Анна</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Сладкие пасхальные яйца 2</a></h3>
+                        <img src="/images/main-slider/author-temp.jpg" alt="">
+                        <p class="author-name">Анна</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Сладкие пасхальные яйца 3</a></h3>
+                        <img src="/images/main-slider/author-temp.jpg" alt="">
+                        <p class="author-name">Анна</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="br-block__item">
+                    <a href="" class="image" style="background-image:url('/images/general-page/pasha-1.jpg')"></a>
+                    <div class="text">
+                        <h3><a href="">Сладкие пасхальные яйца 4</a></h3>
+                        <img src="/images/main-slider/author-temp.jpg" alt="">
+                        <p class="author-name">Анна</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="competition">
+    <img class="left-1" src="/images/recepie-page/ing-left-1.png" alt="">
+    <img class="left-2" src="/images/recepie-page/ing-left-2.png" alt="">
+    <img class="left-3" src="/images/recepie-page/ing-left-3.png" alt="">
+    <img class="right-1" src="/images/recepie-page/ing-right-1.png" alt="">
+    <img class="right-2" src="/images/recepie-page/ing-right-2.png" alt="">
+    <img class="right-3" src="/images/recepie-page/ing-right-3.png" alt="">
+    <div class="competition__wrapper">
+        <h1 class="title">
+            Конкурс!
+        </h1>
+        <div class="competition__text">
+            <h2 class="subtitle">
+                Академия Dr. Oetker объявляет конкурс <br/>
+                на самый красивый пасхальный кулич.  
+            </h2>
+            <p class="text">
+                Готовь куличи, публикуй фотографии под хештегом 
+            </p>
+            <h2 class="subtitle">
+                #DrOetker_Пасха
+            </h2>
+            <p class="text">
+                в своем аккаунте Instagram. Авторы лучших, по мнению жюри, <br/>
+                15 работ получат  пасхальные наборы Dr. Oetker
+            </p>
+            <a href="#" class="link special-link">
+                Полные правила
+            </a>
+            <a href="#" class="link">
+                Все участники
+            </a>
+        </div>
+    </div>
+</div>
